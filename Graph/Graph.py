d6f92ee7121f9d2e72c36b40f2931fbedb180f91
Michael <michael.yang.d@gmail.com>
2018-05-17 03:43:49 -0400

    Graph implementation and BFS

---
+++ Graph/Graph.py
@@ -0,0 +1,36 @@
+#[0, 1, 2, 3,4, 5,6, 7,8,9,10, 11,12,13,14, ...]
+class Vertex:
+	def __init__(self, key):
+		self.key = key
+		self.neighbors = []
+
+	def addNeighbor(self, neighbor):
+		self.neighbors.append(neighbor)
+
+	def getNeighbors(self):
+		return self.neighbors
+	def __repr__(self):
+		return self.key
+	def __str__(self):
+		return self.key
+
+class Graph:
+	def __init__(self):
+		self.verticies = {}
+
+	def addVertex(self, key):
+		self.verticies[key] = Vertex(key)
+
+	def addEdge(self, a, b):
+		if a not in self.verticies:
+			self.addVertex(a)
+		if b not in self.verticies:
+			self.addVertex(b)
+		self.verticies[a].addNeighbor(self.verticies[b])
+		self.verticies[b].addNeighbor(self.verticies[a])
+
+	def getVerticies(self):
+		return self.verticies.values()
+
+	def size(self):
+		return len(self.verticies)
---
+++ Graph/bfs.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+import sys
+import os
+import argparse
+from Graph import *
+
+_defaultTestFile = 'test.txt'
+
+def bfs(graph, v):
+	currentLevel = 0
+	level = { v: currentLevel }
+	frontier = v.neighbors
+	while frontier:
+		currentLevel+=1
+		nextFrontier = []
+		for v in frontier:
+			if v not in level:
+				level[v] = currentLevel
+				for n in v.neighbors:
+					if n not in level and n not in frontier:
+						nextFrontier.append(n)
+		frontier = nextFrontier
+	return level
+
+def translateFile(file = _defaultTestFile):
+	res = []
+	if not os.path.isfile(file):
+		print("No such file: {}".format(file))
+		sys.exit()
+	with open(file) as f:
+		while True:
+			g = Graph()
+			vLine = f.readline()
+			eLine = f.readline()
+			if not eLine:
+				break
+			verticies = map(str, vLine.rstrip().split(' '))
+			edges = map(str, eLine.rstrip().split(', '))
+			for v in verticies:
+				g.addVertex(v)
+			for e in edges:
+				el = e.split()
+				g.addEdge(el[0], el[1])
+			res.append(g)
+	return res
+
+def main():
+	#python insertion_sort.py -f test.txt
+	parser = argparse.ArgumentParser(description='provide test file')
+	parser.add_argument('--file', '-f', type=str, help='name of the file containing test cases', default=_defaultTestFile)
+	args = parser.parse_args()
+	file = args.file
+	testGraphs = translateFile(args.file)
+	results = []
+	for testGraph in testGraphs:
+		verticies = testGraph.getVerticies()
+		for v in verticies:
+			print v
+			print v.neighbors
+		res = bfs(testGraph, verticies[0])
+		print res
+	#	results.append(sort(testCase))
+	#for result in results:
+	#	print result
+
+if __name__ == '__main__':
+	main()
